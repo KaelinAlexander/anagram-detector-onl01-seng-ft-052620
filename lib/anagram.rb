05c05c33e93402a3817209547b0605469e0ef062
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-08 17:23:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+attr_accessor :candidate
+
+def initialize(word)
+  @candidate = word
+end
+
+def match(list)
+  matches = []
+  can_string = candidate.split("").sort
+    list.each do |word|
+      word_array = word.split("").sort
+      if can_string == word_array
+        matches << word
+      end
+    end 
+    matches
+end
+
+end
